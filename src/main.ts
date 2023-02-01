98396c1e80da130c40baa802e0c75fc9da927da4
chrisEsk <chrisesk.dev@gmail.com>
2023-01-31 19:48:42 -0600

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,15 @@ import {
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
-  Validators,
 } from '@angular/forms';
 
 // Escriba un código que permita ingresar valores en una página html (nombre, apellido, edad, ubicación), y al presionar un botón oculte el formulario y muestre la información en pantalla. Considerar un botón “atrás” para ocultar la información y mostrar el formulario. (usa tu editor de código favorito, y cuando tengas la solución pégala acá o déjanos un link para descargarla, así podremos ejecutar el código)
+export interface User {
+  nombre: string;
+  apellido: string;
+  edad: number;
+  ubicacion: string;
+}
 
 @Component({
   selector: 'my-app',
@@ -19,7 +24,7 @@ import {
   styleUrls: ['global_styles.css'],
   template: `
   <div class="container mt-4">
-    <div *ngIf="!showSummary">
+    <div *ngIf="!showSummary; else summary">
       <form [formGroup]="form" (ngSubmit)="onSave()" class="card">
         <div class="card-body">
           <div class="mb-3">
@@ -46,52 +51,52 @@ import {
             </label>
             <textarea formControlName="ubicacion" id="ubicacion" type="text" class="form-control" aria-describedby="ubicacion"></textarea>
           </div>
-          <button class="btn btn-primary" (click)="onSave()" type="submit">Save</button>
+          <button class="btn btn-primary" (click)="onSave()" type="submit">Guardar</button>
         </div>
       </form>
     </div>
-    <div [hidden]="showSummary">
-      <a (click)="goBack()">← Regresar</a>
-
-      <p>Datos del formulario:</p>
-
-      {{this.summaryData}}
-
-    </div>
+    <ng-template #summary>
+      <div class="card">
+        <div class="card-body">
+          <u (click)="goBack()">← Regresar</u>
+          <ul class="mt-4">
+            <li>Nombre: {{summaryData.nombre}}</li>
+            <li>Apellido: {{summaryData.apellido}}</li>
+            <li>Edad: {{summaryData.edad}}</li>
+            <li>Ubicacion: {{summaryData.ubicacion}}</li>
+          </ul>
+        </div>
+      </div>
+    </ng-template>
   </div>
   `,
 })
 export class App {
   public form: FormGroup;
   public showSummary = false;
-  public summaryData;
+  public summaryData: User;
 
   constructor(private formBuilder: FormBuilder) {
-    this.formBuilder.group(
-      {
-        nombre: [''],
-        apellido: [''],
-        edad: [''],
-        ubicacion: [''],
-      },
-      {
-        updateOn: 'change',
-      }
-    );
+    this.form = this.buildForm();
   }
 
-  public get nombre() {
-    return this.form.get('nombre');
+  buildForm() {
+    return this.formBuilder.group({
+      nombre: [''],
+      apellido: [''],
+      edad: [''],
+      ubicacion: [''],
+    });
   }
 
   public onSave(): void {
-    this.summaryData = this.form;
-    // this.summaryData = {
-    //   nombre: this.form.get('nombre').value,
-    //   apellido: this.form.get('apellido'),
-    //   edad: this.form.get('edad'),
-    //   ubicacion: this.form.get('ubicacion'),
-    // };
+    this.showSummary = true;
+    this.summaryData = {
+      nombre: this.form.get('nombre').value,
+      apellido: this.form.get('apellido').value,
+      edad: this.form.get('edad').value,
+      ubicacion: this.form.get('ubicacion').value,
+    };
   }
 
   goBack() {
